a919a5a8246037317a715be0eed44ca3a69b5291
rutyak <rutikkhandekar123@gmail.com>
2025-01-07 04:14:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById("menu");
+let mobileMenu = document.getElementById("mobile-menu");
+
+menu.addEventListener("click",()=>{
+    mobileMenu.classList.toggle("show");
+})
